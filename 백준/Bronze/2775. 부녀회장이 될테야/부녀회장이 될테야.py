11e3542866cfb0a0e9154089ee71a909189ac595
Han Yujin <144632847+hanujin@users.noreply.github.com>
2025-02-21 07:32:56 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    k = int(input())
+    n = int(input())
+    f0 = [i for i in range(1, n+1)]
+
+    for _ in range(k):
+        for i in range(1, n):
+            f0[i] += f0[i-1]
+        
+    print(f0[-1])
